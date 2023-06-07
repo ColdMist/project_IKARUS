e758c559430270ae4234316d263efcd52012dc57
ColdMist <turzo.mohtasim@gmail.com>
2023-06-07 13:28:14 +0200

    files added

--- utils/helper_functions.py
+++ utils/helper_functions.py
@@ -1,8 +1,42 @@
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 import os
+import en_core_web_sm
+import pickle
+import json
+
+def write_to_txt_file(path, data):
+    """
+    :param
+
+    path: path where to be saved
+    data: triples to be written in txt
+
+    """
+    f = open(path, "w")
+    for i in range(data.shape[0]):
+        line = ''
+        for j in range(data.shape[1]):
+            if(j==0):
+                line = str(data[i][j])
+            else:
+                line = line + '\t' + str(data[i][j])
+        f.write(line)
+        f.write("\n")
+        print(line)
+    f.close()
+
+def load_nlp():
+    '''
+    @param:
+    @return: nlp object
+    '''
+    nlp = en_core_web_sm.load()
+    nlp.add_pipe("entityLinker", last=True)
+    return nlp
+
 def setup_openAI():
-    os.environ["OPENAI_API_KEY"] = "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"
+    os.environ["OPENAI_API_KEY"] = "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"
 
 def preprocess_texts(raw_text):
     '''
@@ -51,3 +85,85 @@ def process_all_pdfs(directory_path, preprocess_langchain=False):
             texts = read_pdf_text(filepath, preprocess_langchain)
             all_texts.extend(texts)
     return all_texts
+
+
+def get_linked_entities(nlp, entity):
+    # add pipeline (declared through entry_points in setup.py)
+    all_linked_entities = nlp(entity)._.linkedEntities
+    return all_linked_entities
+
+def store_triples(triple_list, path):
+    with open(path, "w") as file:
+        for triple in triple_list:
+            line = "\t".join(triple)
+            file.write(line + "\n")
+
+def obtain_graph_information(triples):
+    '''
+    @param triples: list of triples
+    @return: dictionary of entities and relations
+    '''
+    entities = set()
+    relations = set()
+    for triple in triples:
+        entities.add(triple[0])
+        entities.add(triple[2])
+        relations.add(triple[1])
+    return entities, relations
+
+def obtain_connection_information(nlp ,entity_list):
+    '''
+    @param nlp: the nlp object
+    @param entity_list:  the list of entities found
+    @return: the dictionary containing entities and their connections
+    '''
+    neighborhood_dict = {}
+    for entity in entity_list:
+        linked_entities_per_entity = get_linked_entities(nlp, str(entity))
+        print(f"entity_label: {entity}")
+        print(f"connected_entity_object: {linked_entities_per_entity}")
+        print('######################')
+        neighborhood_dict[entity] = linked_entities_per_entity
+    return neighborhood_dict
+
+# def load_nlp():
+#     '''
+#     @param:
+#     @return: nlp object
+#     '''
+#     nlp = en_core_web_sm.load()
+#     return nlp
+
+# specify your desired json file path
+def store_to_json(json_file_path, connection_information):
+    '''
+    @param json_file_path: path to store the json file
+    @param connection_information: the connection information
+    @return:
+    '''
+    # open a file for writing
+    with open(json_file_path, 'w') as file:
+        # write the Python dictionary to a JSON file
+        json.dump(connection_information, file)
+
+def store_to_pickle(pickle_file_path, connection_information):
+    '''
+    @param pickle_file_path: the path to pkl file
+    @param connection_information:  the connection information
+    @return: None
+    '''
+    # open a file for writing
+    with open(pickle_file_path, 'wb') as file:
+        # write the Python dictionary to a pickle file
+        pickle.dump(connection_information, file)
+
+def store_triples(triple_list, path):
+    '''
+    @param triple_list: the list of triples to store
+    @param path: the path to store the triples
+    @return: NULL
+    '''
+    with open(path, "w") as file:
+        for triple in triple_list:
+            line = "\t".join(triple)
+            file.write(line + "\n")
